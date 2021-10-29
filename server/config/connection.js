59f66e8a40ae9f739d9919edb82ae0ecfaf75aea
Sophie Deskin <sophiedeskin@gmail.com>
2021-10-29 14:00:24 -0700

    updating server

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,8 +2,9 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI 
-   || 'mongodb+srv://sophiedeskin:Ags3225!@cluster0.aboxa.mongodb.net/googlebooks?retryWrites=true&w=majority'
+  // process.env.MONGODB_URI 
+  //  || 
+   'mongodb+srv://sophiedeskin:Ags3225!@cluster0.aboxa.mongodb.net/googlebooks?retryWrites=true&w=majority'
   , 
     {
     useNewUrlParser: true,
