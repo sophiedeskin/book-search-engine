cc788734c62a7e6cfb96b811c4d70fcd7a8a1ee9
Sophie Deskin <sophiedeskin@gmail.com>
2021-10-28 18:40:17 -0700

    updating server

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ const server = new ApolloServer({
   resolvers,
   context: authMiddleware
 });
+
 server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
@@ -23,11 +24,11 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
